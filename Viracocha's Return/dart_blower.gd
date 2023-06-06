f6ef8cb6ccb972313207c2c382e90264f9dca9a3
jamesbchao <jbchao@stanford.edu>
2023-06-05 22:10:46 -0700

    initial dart blower

--- Viracocha's Return/dart_blower.gd
+++ Viracocha's Return/dart_blower.gd
@@ -1,39 +1,26 @@
 extends CharacterBody2D
 class_name DartBlower
-var Dart = preload("res://flower_dart.tscn")
 
-const SPEED = -20.0
-const JUMP_VELOCITY = -400.0
+var speed = 50
+var patrol_dist = 20
+var direction = Vector2.LEFT
+var dist_traveled = 0
 
 @onready var player = get_tree().get_nodes_in_group("player")[0]
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func shoot():
-	var d = Dart.instantiate()
-	d.global_position = $Mouth.global_position
-	var dir = (player.global_position - $Mouth.global_position).normalized()
-	d.global_rotation = dir.angle()
-	d.direction = dir
-	get_tree().root.add_child(d)
-
+	
 func _physics_process(delta):
-	pass
-#	velocity.x = SPEED
-#
-#	# Add the gravity.
-#	if not is_on_floor():
-#		velocity.y += gravity * delta
-#
-#	var collision = move_and_collide(velocity * delta)
-#	if collision:
-#		velocity = velocity.bounce(collision.get_normal())
-#		var collider = collision.get_collider()
-#		if collider is CharacterBody2D:
-#			print('player hit')
-
-
-
-func _on_shoot_timer_timeout():
-	shoot()
+	var collision = move_and_collide(direction * speed * delta)
+	if collision:
+		var collider = collision.get_collider()
+		if collider is PlayerBody:
+			player.global_position = player.lastCheckpoint
+		direction = -direction
+		dist_traveled = 0
+	
+	dist_traveled += abs(velocity.x)
+	if dist_traveled >= patrol_dist:
+		direction = -direction
+		dist_traveled = 0
